b7c7fc32612b7e84125d8ea280aad38364245c70
Lako <lakoisfun@proton.me>
2024-01-20 17:41:00 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Panel Note
+![screenshot](./images/screen1.png)
+
+Add a small note to your GNOME panel. 
+
+> [!NOTE]
+> This extension is inspired by the macOS app "One Thing."
+
+## Usage
+![screenshot](./images/screen2.png)
+
+When you enable the extension, the default note will appear on your panel. To edit the note, simply click on the note and type in your new note.
---
+++ extension.js
@@ -0,0 +1,87 @@
+/* extension.js
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+// Inspired by the macOS app 'One Thing'
+// Extension uses elements from 'Just Another Search Bar' (https://extensions.gnome.org/extension/5522/just-another-search-bar/)
+
+import GObject from 'gi://GObject';
+import St from 'gi://St';
+import Clutter from 'gi://Clutter';
+
+import { Extension, gettext as _ } from 'resource:///org/gnome/shell/extensions/extension.js';
+import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
+import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
+
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+
+const Indicator = GObject.registerClass(
+    class Indicator extends PanelMenu.Button {
+        _init(settings) {
+            super._init(0.0, _('Panel Note'));
+
+            /* ------------------------------- Panel Note ------------------------------- */
+            let noteInPanel = new St.Label({
+                text: settings.get_string('note'),
+                y_expand: true,
+                y_align: Clutter.ActorAlign.CENTER,
+            });
+            this.add_child(noteInPanel);
+
+
+            /* ----------------------------- Note Entry Box ----------------------------- */
+            this.entry = new St.Entry({
+                text: settings.get_string('note'),
+                can_focus: true,
+                track_hover: true
+            });
+
+            this.entry.set_primary_icon(new St.Icon({
+                icon_name: 'document-edit-symbolic',
+                style_class: 'popup-menu-icon',
+            }));
+
+            this.entry.clutter_text.connect('text-changed', () => {
+                let text = this.entry.get_text();
+                if (text == "")
+                    text = "No Note";
+                settings.set_string('note', text);
+                noteInPanel.text = text;
+            });
+
+            let popupEdit = new PopupMenu.PopupMenuSection();
+            popupEdit.actor.add_actor(this.entry);
+
+            this.menu.addMenuItem(popupEdit);
+            this.menu.actor.add_style_class_name('note-entry');
+        }
+    });
+
+export default class IndicatorExampleExtension extends Extension {
+    enable() {
+        this._settings = this.getSettings();
+        this._indicator = new Indicator(this._settings);
+        Main.panel.addToStatusArea(this.uuid, this._indicator);
+    }
+
+    disable() {
+        this._indicator.entry.disconnect();
+        this._indicator.destroy();
+        this._indicator = null;
+        this._settings = null;
+    }
+}
